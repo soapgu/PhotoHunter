8e61d679955bbbd4edfd926f58971255723051d6
guhui <guhui@shgbit.com>
2021-02-24 23:01:23 +0800

    1 temp store image in cache 2 onSaveInstanceState save photo info json

--- app/src/main/java/com/soapdemo/photohunter/MainActivity.java
+++ app/src/main/java/com/soapdemo/photohunter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.soapdemo.photohunter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -15,6 +16,9 @@ import com.soapdemo.photohunter.models.Photo;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -29,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
     private String url = "https://api.unsplash.com/photos/random?client_id=ki5iNzD7hebsr-d8qUlEJIhG5wxGwikU71nsqj8PcMM";
     private static OkHttpClient client = new OkHttpClient();
+    Gson gson = new Gson();
     TextView textView;
     ImageView photoView;
+    String currentPhotoJson;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -39,12 +45,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = this.findViewById((R.id.msg_view));
         photoView = this.findViewById(R.id.photo_view);
+        if( savedInstanceState != null ) {
+            String savedJson = savedInstanceState.getString("Json");
+            if( savedJson != null ) {
+                this.currentPhotoJson = savedJson;
+                Photo photo = gson.fromJson(savedJson, Photo.class);
+                File file = new File(getExternalCacheDir(), String.format( "%s.jpg" ,photo.id ) );
+                Bitmap bitmap = BitmapFactory.decodeFile(file.getPath());
+                runOnUiThread(() -> photoView.setImageBitmap(bitmap));
+            }
+        }
         this.findViewById(R.id.new_button).setOnClickListener( v ->{
             Request request = new Request.Builder()
                     .url(url)
                     .get()
                     .build();
-            Call call = this.client.newCall(request);
+            Call call = client.newCall(request);
 
             call.enqueue(new Callback() {
                 @Override
@@ -58,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
                     Logger.i("Response Code:%s",response.code());
                     if( response.isSuccessful() ) {
                         Logger.i("Success fetch photo information , Thread:%s" , Thread.currentThread().getId());
-                        Gson gson = new Gson();
                         try( ResponseBody responseBody = response.body() ) {
-                            Photo photo = gson.fromJson(responseBody.string(), Photo.class);
+                            String jsonString = responseBody.string();
+                            Photo photo = gson.fromJson(jsonString, Photo.class);
                             runOnUiThread(() -> textView.setText(photo.alt_description));
 
                             Request imageRequest = new Request.Builder()
@@ -78,10 +94,30 @@ public class MainActivity extends AppCompatActivity {
                                 public void onResponse(@NotNull Call call, @NotNull Response response) {
                                     if (response.isSuccessful()) {
                                         Logger.i("Success response  photo image resource , Thread:%s" , Thread.currentThread().getId());
+                                        FileOutputStream fos = null;
                                         try( ResponseBody downloadBody = response.body() ) {
                                             InputStream inputStream = downloadBody.byteStream();
-                                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                            byte[] buf = new byte[2048];
+                                            File file = new File(getExternalCacheDir(), String.format( "%s.jpg" ,photo.id ) );
+                                            fos = new FileOutputStream(file);
+                                            int len;
+                                            while ((len = inputStream.read(buf)) != -1) {
+                                                fos.write(buf, 0, len);
+                                            }
+                                            fos.flush();
+                                            Bitmap bitmap = BitmapFactory.decodeFile(file.getPath());
                                             runOnUiThread(() -> photoView.setImageBitmap(bitmap));
+                                            currentPhotoJson = jsonString;
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        } finally {
+                                            if (fos != null) {
+                                                try {
+                                                    fos.close();
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
                                         }
                                     }
                                 }
@@ -92,5 +128,14 @@ public class MainActivity extends AppCompatActivity {
             });
             textView.setText( "Requesting..." );
         } );
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if( this.currentPhotoJson != null ) {
+            //Logger.i("Save Photo Json: %s", this.currentPhotoJson);
+            outState.putString("Json", this.currentPhotoJson);
+        }
     }
 }
