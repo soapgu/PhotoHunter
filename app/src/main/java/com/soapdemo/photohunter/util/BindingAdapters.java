db65ba137d22b253fc972a05ee2b435313cedc2f
guhui <guhui@shgbit.com>
2021-03-22 16:12:58 +0800

    add cardview and glide to photo list

--- app/src/main/java/com/soapdemo/photohunter/util/BindingAdapters.java
+++ app/src/main/java/com/soapdemo/photohunter/util/BindingAdapters.java
@@ -1,12 +1,15 @@
 package com.soapdemo.photohunter.util;
 
 import android.graphics.Bitmap;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import androidx.annotation.LayoutRes;
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.soapdemo.photohunter.adapters.ShadowAdapter;
 
 import java.util.List;
@@ -16,6 +19,16 @@ public class BindingAdapters {
     @BindingAdapter("imageBitmap")
     public static void loadImage(ImageView iv, Bitmap bitmap) {
         iv.setImageBitmap(bitmap);
+    }
+
+    @BindingAdapter("imageFromUrl")
+    public  static void  loadGlide( ImageView imageView , String imageUrl ){
+        if (!TextUtils.isEmpty(imageUrl) ) {
+            Glide.with(imageView.getContext())
+                    .load(imageUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageView);
+        }
     }
 
     @BindingAdapter(value = {"itemsSource","itemTemplate","variableName"},requireAll = false)
